44ce24f3ed4622afef9e35119f6261964504c5cf
Aniket Ghosh <ghoshaniket1000@gmail.com>
2024-03-21 01:06:19 +0530

    Cerated Quote component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,19 @@
-
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Signin } from "./pages/Signin";
+import { Signup } from "./pages/Signup";
+import { Blog } from "./pages/Blog";
 function App() {
 
   return (
-    <div>
-      
-    </div>
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/signup" element={<Signup />}/>
+          <Route path="/signin" element={<Signin />}/>
+          <Route path="/blog/:id" element={<Blog />}/>
+        </Routes>
+      </BrowserRouter>
+    </>
   )
 }
 
---
+++ frontend/src/components/Quote.tsx
@@ -0,0 +1,17 @@
+export const Quote = () => {
+    return (
+        <div className="bg-gray-100 h-screen flex flex-col justify-center items-center">
+            <div className="w-full px-4 sm:max-w-sm md:max-w-md lg:max-w-lg xl:max-w-xl 2xl:max-w-2xl">
+                <div className="font-bold text-4xl font-sans-serif">            
+                    “There will be a good quote in the future. By reading this you are now a supiriour human”
+                </div>
+                <div className="px-2 pt-4 font-semibold text-lg font-ui-sans-serif antialiased">
+                    Aniket Ghosh
+                </div>
+                <div className="px-2 font-semibold text-sm text-gray-500">
+                    CEO | SSiT
+                </div>
+            </div>
+        </div>
+    )
+}
